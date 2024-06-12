46646ed27103f046845e1a545a714c1f121ec38f
reclib <reclib.microempresa@gmail.com>
2024-06-12 16:46:18 -0600

    formulario

--- contacto.php
+++ contacto.php
@@ -5,11 +5,15 @@
   $bd = "contacto";
   $enlace = mysqli_connect ($servidor, $usuario, $clave, $bd);
 ?>
-<br><br>
-<center><p class="formTitle">Registro</p></center>
-  <br><br><hr>
-  <form action="contacto.php" method="POST">
-    <div id="Regisitro"><br>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <br><br>
+    <center><p class="formTitle">Registro</p></center>
+    <br><br><hr>
+    <form action="contacto.php" method="POST">
       <center><table cellspacing="30px">
         <tr>
           <td><label>Nombre Completo</label></td>
@@ -33,13 +37,14 @@
           </tr>
         <tr></tr><tr></tr>
         <tr>
-          <td><label><center><input type="submit" value="Enviar"></center></label></td>
+          <td><label><center><input type="submit" name="enviar" placeholder="Enviar"></center></label></td>
           <td><label><center><input type="reset" value="Restablecer"></center></label></td>
         </tr>
           </center>
       </table>
-    </div>
-  </form>
+    </form>
+  </body>
+</html>
 <?php
   if(isset($_POST['enviar'])){
     $Nombre = $_POST['nombre'];
